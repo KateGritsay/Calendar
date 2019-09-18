57ef044d9be3610fcccc371408db1e6ebc4adf1d
KateGritsay <fk0306@mail.ru>
2019-09-18 15:31:04 +0300

    refactoring pkg calendarpb

--- internal/pkg/calendar/calendar.go
+++ internal/pkg/calendar/calendar.go
@@ -1,5 +1,5 @@
 
-//go:generate protoc --go_out=. calendar.proto
+//go:generate protoc --go_out=. calendarpb.proto
 
 
 package calendar
@@ -7,40 +7,40 @@ package calendar
 import (
 	"errors"
 	"fmt"
-	""
+	calendar "github.com/KateGritsay/Calendar/pkg/calendar"
 )
 
 type Calendar struct {
-	events map[uint64]Event
+	events map[uint64]calendar.Event
 }
 
 func NewCalendar() Calendar {
-	cal := Calendar{events: make(map[uint64]Event)}
+	cal := Calendar{events: make(map[uint64]calendar.Event)}
 	return cal
 }
 
-func (cal *Calendar) AddEvent(event Event) Event {
+func (c *Calendar) AddEvent(event calendar.Event) calendar.Event {
 	var counter uint64
 	counter++
 	event.UUID = counter
-	cal.events[event.UUID] = event
+	c.events[event.UUID] = event
 
 	return event
 }
 
-func (cal *Calendar) DeleteEvent(event Event) error {
-	_, ok := cal.events[event.UUID]
+func (c *Calendar) DeleteEvent(event calendar.Event) error {
+	_, ok := c.events[event.UUID]
 	if ok {
-		delete(cal.events, event.UUID)
+		delete(c.events, event.UUID)
 	}
 	return errors.New(fmt.Sprintf("Can't delete event: UUID %d not found", event.UUID))
 
 }
 
-func (cal *Calendar) ReplaceEvent(event Event) error {
-	_, ok := cal.events[event.UUID]
+func (c *Calendar) ReplaceEvent(event calendar.Event) error {
+	_, ok := c.events[event.UUID]
 	if ok {
-		cal.events[event.UUID] = event
+		c.events[event.UUID] = event
 	}
 	return errors.New(fmt.Sprintf("UUID %d not found", event.UUID))
 }
--- server/server.go
+++ server/server.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"context"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	"log"
 	"net"
 	"github.com/KateGritsay/Calendar/pkg/calendar"
-	"github.com/KateGritsay/Calendar/internal/calendar"
+	"github.com/KateGritsay/Calendar/pkg/"
 	"time"
 )
 
@@ -24,16 +25,21 @@ type calendarServer struct{}
 
 calendarpb.RegisterCalendarServer(grpcServer, &calendarServer{})
 grpcServer.Serve(lis)
+
 }
 
-func mapEventpbToEvent(event *calendarpb.Event) *calendar.Event {
-	return &calendar.Event{
-		UUID:        event.UUID,
-		UserId:      event.UserId,
-		Description: event.Description,
-		End:         time.Unix(event.End.Seconds, int64(event.End.Nanos)),
-		Start:       time.Unix(event.Start.Seconds, int64(event.Start.Nanos)),
-		NoticeTime:  event.NoticeTime,
-		Title:       event.Title,
+func (s *calendarServer) CreateEvent (ctx context.Context, req calendarpb.CreateEventReq) (*calendarpb.CreateEventRes, error){
+	event := mapEventpbToEvent(req.GetEvent())
+
+	id, err := s.service.AddEvent(ctx, *event)
+
+	if err != nil {
+		return &calendarpb.CreateEventRes{Error: err.Error()}, nil
 	}
+
+	return &calendarpb.CreateEventRes{UUID: id}, nil
+
+
 }
+
+func ToEvent (event *calendarpb.Event) *calendar.Event

1cc0da5174e16186643bd9229b83570ec5cfb16b
KateGritsay <fk0306@mail.ru>
2019-09-18 12:12:05 +0300

    refactoring package

--- server/server.go
+++ server/server.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 	"log"
 	"net"
-	""
+	"github.com/KateGritsay/Calendar/pkg/calendar"
+
 )
 
 type calendarServer struct{}
+
+func (s *calendarServer) CreateEvent(ctx context.Context, req *calendar.CreateEventReq) (*CreateEventRes, error) {
+}
 
 	func main() {
 	lis, err := net.Listen("tcp", "0.0.0.0:50051")
@@ -15,7 +20,8 @@ type calendarServer struct{}
 		log.Fatalf("failed to listen %v", err)
 	}
 
-grpcServer := grpc.NewServer()
+		grpcServer := grpc.NewServer()
+		reflection.Register(grpcServer)
 
 calendarpb.RegisterCalendarServer(grpcServer, &calendarServer{})
 grpcServer.Serve(lis)
